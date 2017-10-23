9a9da4822bb48cfe4e2342cabe5456ab4c8e9ebe
huynhthaisang95 <sangsangsuot1995@gmail.com>
2017-10-23 15:19:36 +0700

    Update

---
+++ WebXe/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebXe.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Xe()
+        {
+            return View();
+        }
+
+        public ActionResult Themmoixe()
+        {
+            return View();
+        }
+
+        public ActionResult Chinhsua()
+        {
+            return View();
+        }
+    }
+}
